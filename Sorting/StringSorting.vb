8ee1bf0502ebaa73c953c43f4b82e5392d366fe3
Gabriel Nadler <nadlergabriel@gmail.com>
2013-01-13 13:08:08 +0100

    Dokumentation Fertig. Removed Some Comments, Renamed some variables according to stupid naming-scheme.

--- Sorting/StringSorting.vb
+++ Sorting/StringSorting.vb
@@ -1,18 +1,11 @@
-﻿Public Class StringSorting 'Rename??
-    'Inherits ArrayList
+﻿Public Class StringSorting 
     Private blnUseWords As Boolean '
     Private strWords As String() ' The objects to be sorted
     Private blnAlive As Boolean = True ' This needs to be here, it doesn't work when saved in the form. Makes Functions abort when false
     Private alstList As ArrayList
 
-    Public Sub kill()
-        blnAlive = False
-    End Sub
 
-    Public Function Length() As Integer
-        Return alstList.ToArray.Length
-    End Function
-
+    'Constructor
     Public Sub New(ByVal strText As String, Optional ByVal blnWords As Boolean = False)
         Me.blnUseWords = blnWords
         alstList = New ArrayList
@@ -34,32 +27,17 @@
         ReDim Preserve strWords(alstList.ToArray.Length - 1)
         alstList.ToArray.CopyTo(strWords, 0)
     End Sub
-
-
-    Public Overrides Function ToString() As String
-        alstList.Clear()
-        alstList.AddRange(strWords)
-        Dim strReturn As String = ""
-        Dim e As IEnumerator
-        e = alstList.GetEnumerator()
-        While (e.MoveNext())
-            Dim objWord As Object = e.Current
-            strReturn &= CStr(objWord)
-            If Me.blnUseWords Then
-                strReturn &= " "
-            End If
-        End While
-        Return strReturn
-    End Function
-
-
+	
+	
+	'Sorting Functions
+	
     Public Sub Insertionsort()
         Dim intLength As Integer = strWords.Length
         Dim intLeft As Integer = 0
         Do
             Dim intMax As Integer = intLeft
             For i = intLeft + 1 To intLength - 1
-                If StrComp(maskUmlaute(strWords(i)), maskUmlaute(strWords(intMax)), optionCompare()) = -1 Then
+                If StrComp(maskUmlaute(strWords(i)), maskUmlaute(strWords(intMax)), compareMethod()) = -1 Then
                     intMax = i
                 End If
                 If Not blnAlive Then ' Make it correctly quit when quitting :)
@@ -79,7 +57,7 @@
         Do
             swapped = False
             For i = 0 To length - 2
-                If StrComp(maskUmlaute(strWords(i)), maskUmlaute(strWords(i + 1)), optionCompare()) = 1 Then
+                If StrComp(maskUmlaute(strWords(i)), maskUmlaute(strWords(i + 1)), compareMethod()) = 1 Then
                     SwapValues(i, i + 1)
                     swapped = True
                 End If
@@ -98,7 +76,7 @@
         Do
             swapped = False
             For i = 0 To length - 2
-                If StrComp(maskUmlaute(strWords(i)), maskUmlaute(strWords(i + 1)), optionCompare()) = 1 Then
+                If StrComp(maskUmlaute(strWords(i)), maskUmlaute(strWords(i + 1)), compareMethod()) = 1 Then
                     SwapValues(i, i + 1)
                     swapped = True
                 End If
@@ -120,10 +98,10 @@
         Dim x As String = strWords((intLeft + intRight) \ 2)
         While i <= j
 
-            While StrComp(maskUmlaute(strWords(i)), maskUmlaute(x), optionCompare()) = -1
+            While StrComp(maskUmlaute(strWords(i)), maskUmlaute(x), compareMethod()) = -1
                 i += 1
             End While
-            While StrComp(maskUmlaute(x), maskUmlaute(strWords(j)), optionCompare()) = -1
+            While StrComp(maskUmlaute(x), maskUmlaute(strWords(j)), compareMethod()) = -1
                 j -= 1
             End While
             If i <= j Then
@@ -146,6 +124,7 @@
 
     End Sub
 
+    'Utility Functions
 
     Private Sub SwapValues(ByVal intFirst As Integer, ByVal intSecond As Integer)
         Application.DoEvents() ' Make the program responsive (in here because all sort-functions use this)
@@ -193,12 +172,39 @@
         Return strWord
     End Function
 
-    Private Function optionCompare() As CompareMethod
-        If frmMain.optionCompare() Then
+
+
+    Public Overrides Function ToString() As String
+        alstList.Clear()
+        alstList.AddRange(strWords)
+        Dim strReturn As String = ""
+        Dim e As IEnumerator
+        e = alstList.GetEnumerator()
+        While (e.MoveNext())
+            Dim objWord As Object = e.Current
+            strReturn &= CStr(objWord)
+            If Me.blnUseWords Then
+                strReturn &= " "
+            End If
+        End While
+        Return strReturn
+    End Function
+
+
+    Private Function compareMethod() As CompareMethod
+        If frmMain.textCompare() Then
             Return CompareMethod.Text
         Else
             Return CompareMethod.Binary
         End If
+    End Function
+	
+	    Public Sub kill()
+        blnAlive = False
+    End Sub
+
+    Public Function Length() As Integer
+        Return alstList.ToArray.Length
     End Function
 
 End Class
--- Sorting/frmMain.vb
+++ Sorting/frmMain.vb
@@ -1,56 +1,33 @@
 ﻿
 Public Class frmMain
-
-
-    Public alive As Boolean = True
-
+    'Variables
     Private blnSortWords As Boolean = False
     Private blnCompareText As Boolean = False
     Private blnUmlaute As Boolean = False
+    Private udtSortContainer As StringSorting = New StringSorting("")
 
+    'Load
+    Private Sub frmMain_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        mnuSortieren.ShortcutKeys = Keys.Control Or Keys.Enter ' Not possible via GUI-Designer
+        chkWords.Checked = blnSortWords
+        mnuWorte.Checked = blnSortWords
+        chkCompareText.Checked = blnCompareText
 
-    Private udtList As StringSorting = New StringSorting("")
+    End Sub
 
-    Private Sub btnQuit_Click(sender As Object, e As EventArgs) Handles btnQuit.Click
-        udtList.kill()
+    'Click and other Handlers
+    Private Sub btnQuit_Click(sender As Object, e As EventArgs) Handles btnQuit.Click, mnuExit.Click
+        udtSortContainer.kill()
         Me.Close()
     End Sub
 
 
-    Private Sub btnSort_Click(sender As Object, e As EventArgs) Handles btnSort.Click
+    Private Sub btnSort_Click(sender As Object, e As EventArgs) Handles btnSort.Click, mnuSortieren.Click, tlbSort.Click
         If cmbAlgorithm.SelectedItem = "" Then
             MsgBox("Please select an algorithm")
         Else
             sort()
         End If
-    End Sub
-
-
-    Private Sub sort()
-        txtSorted.Text = ""
-        Me.Cursor = Cursors.WaitCursor
-        Dim strText = txtUnsorted.Text.ToCharArray()
-        'Dim udtList As StringList
-        If chkWords.CheckState = 1 Then
-            udtList = New StringSorting(txtUnsorted.Text, True)
-        Else
-            udtList = New StringSorting(txtUnsorted.Text)
-        End If
-        Dim startTime As DateTime = Now
-        Select Case cmbAlgorithm.SelectedItem
-            Case "Bubblesort"
-                udtList.BubbleSort()
-            Case "Insertionsort"
-                udtList.Insertionsort()
-            Case "Quicksort"
-                udtList.Quicksort()
-            Case "Ripplesort"
-                udtList.RippleSort()
-        End Select
-        txtDuration.Text = Now.Subtract(startTime).ToString()
-        txtWordNum.Text = udtList.Length()
-        txtSorted.Text = udtList.ToString()
-        Me.Cursor = Cursors.Arrow
     End Sub
 
     Private Sub txtUnsorted_KeyDown(sender As Object, e As KeyEventArgs) Handles txtUnsorted.KeyDown
@@ -59,13 +36,8 @@ Public Class frmMain
         End If
     End Sub
 
-
-    Private Sub BeendenToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles mnuExit.Click
-        btnQuit_Click(sender, e)
-    End Sub
-
     Private Sub NeuToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles mnuNew.Click, tlbNew.Click
-        udtList.kill()
+        udtSortContainer.kill()
         txtSorted.Text = ""
         txtUnsorted.Text = ""
     End Sub
@@ -75,27 +47,6 @@ Public Class frmMain
         mnuWorte.Checked = blnSortWords
         chkWords.Checked = blnSortWords
     End Sub
-
-
-
-    Private Sub frmMain_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        mnuSortieren.ShortcutKeys = Keys.Control Or Keys.Enter
-        chkWords.Checked = blnSortWords
-        mnuWorte.Checked = blnSortWords
-        chkCompareText.Checked = blnCompareText
-
-    End Sub
-
-
-
-    Public Function optionCompare() As Boolean
-        Return blnCompareText
-    End Function
-
-
-    Public Function maskUmlaute() As Boolean
-        Return blnUmlaute
-    End Function
 
     Private Sub chkUmlaute_Click(sender As Object, e As EventArgs) Handles chkCompareText.Click, mnuCompareText.Click
         blnCompareText = Not blnCompareText
@@ -107,17 +58,6 @@ Public Class frmMain
         blnUmlaute = Not blnUmlaute
         mnuUmlaute.Checked = blnUmlaute
         chkUmlaute.Checked = blnUmlaute
-    End Sub
-
-    Private Sub setAlgorithm(ByVal strAlgo As String)
-        cmbAlgorithm.SelectedItem = strAlgo
-        For Each s As ToolStripMenuItem In mnuAlgorithmus.DropDownItems
-            s.Checked = False
-            'Remove the preceding & from the Text
-            If s.Text.Substring(1) = strAlgo Then
-                s.Checked = True
-            End If
-        Next
     End Sub
 
     Private Sub cmbAlgorithm_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cmbAlgorithm.SelectedIndexChanged
@@ -148,21 +88,14 @@ Public Class frmMain
 
     End Sub
 
-    Private Sub mnuSortieren_Click(sender As Object, e As EventArgs) Handles mnuSortieren.Click, tlbSort.Click
-        btnSort_Click(sender, e)
-    End Sub
-
 
     Private Sub mnuCopy_Click(sender As Object, e As EventArgs) Handles mnuCopy.Click, tlbCopy.Click
         If TypeOf Me.ActiveControl Is TextBox Then
-            'Clipboard.SetDataObject(CType(Me.ActiveControl, TextBox).SelectedText)
             CType(Me.ActiveControl, TextBox).Copy()
         End If
     End Sub
     Private Sub mnuCut_Click(sender As Object, e As EventArgs) Handles mnuCut.Click, tlbCut.Click
         If TypeOf Me.ActiveControl Is TextBox Then
-            'Clipboard.SetDataObject(CType(Me.ActiveControl, TextBox).SelectedText)
-            'CType(Me.ActiveControl, TextBox).Clear()
             CType(Me.ActiveControl, TextBox).Cut()
         End If
     End Sub
@@ -170,7 +103,6 @@ Public Class frmMain
     Private Sub mnuPaste_Click(sender As Object, e As EventArgs) Handles mnuPaste.Click, tlbPaste.Click
         If TypeOf Me.ActiveControl Is TextBox And Not CType(Me.ActiveControl, TextBox).ReadOnly Then
             CType(Me.ActiveControl, TextBox).Paste()
-            'CType(Me.ActiveControl, TextBox).SelectedText = Clipboard.GetDataObject.GetData(DataFormats.Text)
         End If
 
     End Sub
@@ -198,8 +130,7 @@ Public Class frmMain
         End If
     End Sub
 
-    Private Sub show_hide_Items_Handler(sender As Object, e As EventArgs) Handles mnuEdit.DropDownOpening, txtUnsorted.Click, txtSorted.Click,
-        chkWords.Click, chkCompareText.Click, chkUmlaute.Click, btnSort.Click
+    Private Sub show_hide_Items_Handler(sender As Object, e As EventArgs) Handles mnuEdit.DropDownOpening, txtUnsorted.Click, txtSorted.Click, chkWords.Click, chkCompareText.Click, chkUmlaute.Click, btnSort.Click
         If TypeOf Me.ActiveControl Is TextBox Then
             mnuCut.Enabled = True
             mnuCopy.Enabled = True
@@ -228,5 +159,62 @@ Public Class frmMain
     Private Sub mnuAbout_Click(sender As Object, e As EventArgs) Handles mnuAbout.Click, tlbHelp.Click
         AboutBox1.Show()
     End Sub
+
+
+    ' Utility Functions
+
+
+    Private Sub sort()
+        txtSorted.Text = ""
+        Me.Cursor = Cursors.WaitCursor
+        If chkWords.CheckState = 1 Then
+            udtSortContainer = New StringSorting(txtUnsorted.Text, True)
+        Else
+            udtSortContainer = New StringSorting(txtUnsorted.Text)
+        End If
+        Dim startTime As DateTime = Now
+        Select Case cmbAlgorithm.SelectedItem
+            Case "Bubblesort"
+                udtSortContainer.BubbleSort()
+            Case "Insertionsort"
+                udtSortContainer.Insertionsort()
+            Case "Quicksort"
+                udtSortContainer.Quicksort()
+            Case "Ripplesort"
+                udtSortContainer.RippleSort()
+        End Select
+        txtDuration.Text = Now.Subtract(startTime).ToString()
+        txtWordNum.Text = udtSortContainer.Length()
+        txtSorted.Text = udtSortContainer.ToString()
+        Me.Cursor = Cursors.Arrow
+    End Sub
+
+    
+
+
+
+    Public Function textCompare() As Boolean
+        Return blnCompareText
+    End Function
+
+
+    Public Function maskUmlaute() As Boolean
+        Return blnUmlaute
+    End Function
+
+   
+
+    Private Sub setAlgorithm(ByVal strAlgo As String)
+        cmbAlgorithm.SelectedItem = strAlgo
+        For Each s As ToolStripMenuItem In mnuAlgorithmus.DropDownItems
+            s.Checked = False
+            'Remove the preceding & from the Text
+            If s.Text.Substring(1) = strAlgo Then
+                s.Checked = True
+            End If
+        Next
+    End Sub
+
+    
 
 End Class
